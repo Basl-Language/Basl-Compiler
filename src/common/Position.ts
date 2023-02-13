c39f29efb3328310abde4d4127adb14f090600a4
mauro-balades <mauro.balades@tutanota.com>
2023-02-13 17:31:24 +0100

    Created lexer public functions

---
+++ src/common/Position.ts
@@ -0,0 +1,10 @@
+
+export default class {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/lexer/index.ts
+++ src/lexer/index.ts
@@ -1,4 +1,22 @@
 
-export default class /* Lexer */ {
-    // TODO:
+import { Source } from "../common/Source";
+import SrcObject from "../common/SrcObject";
+import { Token } from "./tokens";
+
+
+export default class /* Lexer */ extends SrcObject {
+    private _tokens: Token[] = [];
+    constructor(src: Source) {
+        super(src);
+
+
+    }
+
+    public tokenize() {
+
+    }
+
+    public get tokens() {
+        return this._tokens;
+    }
 }
